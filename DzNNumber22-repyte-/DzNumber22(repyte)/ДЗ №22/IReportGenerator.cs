984d7287af6372302694d912c483fb11a9d2d45c
blacktea123409874567 <150480352+blacktea123409874567@users.noreply.github.com>
2026-01-22 18:20:47 +0300

    Add files via upload

---
+++ DzNNumber22-repyte-/DzNumber22(repyte)/ДЗ №22/IReportGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ДЗ__22
+{
+    public delegate void EmployeeHandler(Employee employee);
+
+    public interface IReportGenerator
+    {
+        void GenerateReport();
+    }
+}
---
+++ DzNNumber22-repyte-/DzNumber22(repyte)/ДЗ №22/Program.cs
@@ -0,0 +1,17 @@
+﻿using ДЗ__22;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        EmployeeManager manager = new EmployeeManager();
+
+        manager.AddEmployee(new Employee("Ivan vasilevich", Department.IT, 60000));
+        manager.AddEmployee(new Employee("streve jobs", Department.HR, 50000));
+        manager.AddEmployee(new Employee("kto to", Department.Finance, 70000));
+        manager.AddEmployee(new Employee("qwed afeaef", Department.Marketing, 55000));
+        manager.AddEmployee(new Employee("dMITRY MENDELEEV  ", Department.IT, 65000));
+
+        manager.GenerateReport();
+    }
+}
